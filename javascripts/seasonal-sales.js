8a2fd6e038741d1e168b0b178d2c29692032764b
Palmer Harrell <palmerharrell@me.com>
2016-02-06 12:59:51 -0600

    Made a plan for applying seasonal discounts when selected

--- javascripts/seasonal-sales.js
+++ javascripts/seasonal-sales.js
@@ -1,8 +1,5 @@
-// You work as a developer for a big box store. Here is a JSON representation 
-// of a small sample of the products you sell.
 
-// Your job is to build a web page that lists all of the products, the name of the department 
-// it's in, and the price. Additionally, put a <select> element at the top of the page that 
+// Additionally, put a <select> element at the top of the page that 
 // contains all possible values of the season_discount key in the categories file. As soon 
 // as you select one of the seasons, all prices on the page should immediately be discounted 
 // by the corresponding percentage.
@@ -45,7 +42,7 @@ function productsLoad() {
 	if(products.status === 200) {
 		var productsData = JSON.parse(this.responseText);
 		productsArray = productsData.products;
-		listProducts();
+		checkLoaded();
 	} else {
 		console.log("Error: ", products.status);
 	}
@@ -55,34 +52,47 @@ function categoriesLoad() {
 	if(categories.status === 200) {
 		var categoriesData = JSON.parse(this.responseText);
 		categoriesArray = categoriesData.categories;
-		listProducts();
+		checkLoaded();
 	} else {
 		console.log("Error: ", categories.status);
 	}
 }
 
-function listProducts() {
+function checkLoaded() {
 	// Once both JSON files have been parsed, list products on page
 	if (filesLoaded) {
-		var productHTML = "";
-		for (var i = 0; i < productsArray.length; i++) {
-			
-			// Look up object by property value
-			var category = categoriesArray.filter(function(obj) {
-	  		return obj.id == productsArray[i].category_id;
-			});
-
-			// Construct HTML string for each product
-			productHTML += `<div class="product"><p class="name">${productsArray[i].name}`;
-			productHTML += `</p>`;
-			productHTML += `<p class="dept">${category[0].name}</p>`;
-			productHTML += `<p class="price">${productsArray[i].price}</p></div>`;
-
-		};
-		productsEl.innerHTML += productHTML;	
+		listProducts();
 	} else {
 		filesLoaded = true;
 	}
+}
+
+//							~~~			~~~		 	TO DO 		~~~			~~~
+
+// Figure out how to apply discount (and in which function)
+//			a. var discount = categoriesArray.filter(function(obj) {
+//					return obj.season_discount == seasonChooser.value})
+//			b. var discountPrice = productsArray[i].price * discount
+//			c. Call listProducts() when select value changes (addEventListener)
+
+
+function listProducts() {
+	var productHTML = "";
+	for (var i = 0; i < productsArray.length; i++) {
+		
+		// Look up object by property value
+		var category = categoriesArray.filter(function(obj) {
+  		return obj.id == productsArray[i].category_id;
+		});
+
+		// Construct HTML string for each product
+		productHTML += `<div class="product"><p class="name">${productsArray[i].name}`;
+		productHTML += `</p>`;
+		productHTML += `<p class="dept">${category[0].name}</p>`;
+		productHTML += `<p class="price">${productsArray[i].price}</p></div>`;
+
+	};
+	productsEl.innerHTML += productHTML;	
 }
 
 
