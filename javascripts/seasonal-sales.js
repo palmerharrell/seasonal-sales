e6ff671b625da1dbb5c3f885fdc58ffa2515fc12
Palmer Harrell <palmerharrell@me.com>
2016-02-07 10:38:37 -0600

    Finished Seasonal Sales exercise

--- javascripts/seasonal-sales.js
+++ javascripts/seasonal-sales.js
@@ -1,13 +1,3 @@
-
-// Additionally, put a <select> element at the top of the page that 
-// contains all possible values of the season_discount key in the categories file. As soon 
-// as you select one of the seasons, all prices on the page should immediately be discounted 
-// by the corresponding percentage.
-
-// For example, when Spring is chosen, all products in the corresponding Household category 
-// should have their prices updated with a 15% discount off the base price.
-
-
 var seasonChooser = document.getElementById("seasonChooser");
 var productsEl = document.getElementById("products");
 
@@ -42,7 +32,7 @@ function productsLoad() {
 	if(products.status === 200) {
 		var productsData = JSON.parse(this.responseText);
 		productsArray = productsData.products;
-		checkLoaded();
+		checkIn();
 	} else {
 		console.log("Error: ", products.status);
 	}
@@ -52,13 +42,13 @@ function categoriesLoad() {
 	if(categories.status === 200) {
 		var categoriesData = JSON.parse(this.responseText);
 		categoriesArray = categoriesData.categories;
-		checkLoaded();
+		checkIn();
 	} else {
 		console.log("Error: ", categories.status);
 	}
 }
 
-function checkLoaded() {
+function checkIn() {
 	// Once both JSON files have been parsed, list products on page
 	if (filesLoaded) {
 		listProducts();
@@ -67,34 +57,41 @@ function checkLoaded() {
 	}
 }
 
-//							~~~			~~~		 	TO DO 		~~~			~~~
-
-// Figure out how to apply discount (and in which function)
-//			a. var discount = categoriesArray.filter(function(obj) {
-//					return obj.season_discount == seasonChooser.value})
-//			b. var discountPrice = productsArray[i].price * discount
-//			c. Call listProducts() when select value changes (addEventListener)
-
+seasonChooser.addEventListener("change", function(){
+	listProducts();
+});
 
 function listProducts() {
 	var productHTML = "";
 	for (var i = 0; i < productsArray.length; i++) {
 		
-		// Look up object by property value
-		var category = categoriesArray.filter(function(obj) {
+		// Look up objects by property values to get department & discount
+		var currentCategory = categoriesArray.filter(function(obj) {
   		return obj.id == productsArray[i].category_id;
 		});
 
+		if (seasonChooser.value !== "None") {
+			var selectedDiscount = categoriesArray.filter(function(obj) {
+				return obj.season_discount == seasonChooser.value;
+			});
+		}
+
+		var discountPrice = 0;
+
+		if (seasonChooser.value !== "None") {
+			if (productsArray[i].category_id == selectedDiscount[0].id) {
+				discountPrice = productsArray[i].price - (productsArray[i].price * selectedDiscount[0].discount);
+			} else {
+					discountPrice = productsArray[i].price;
+				};
+	} else {
+		discountPrice = productsArray[i].price;
+	};
+
 		// Construct HTML string for each product
 		productHTML += `<div class="product"><p class="name">${productsArray[i].name}`;
-		productHTML += `</p>`;
-		productHTML += `<p class="dept">${category[0].name}</p>`;
-		productHTML += `<p class="price">${productsArray[i].price}</p></div>`;
-
+		productHTML += `</p><p class="dept">${currentCategory[0].name}</p>`;
+		productHTML += `<p class="price">${discountPrice.toFixed(2)}</p></div>`;
 	};
-	productsEl.innerHTML += productHTML;	
+	productsEl.innerHTML = productHTML;	
 }
-
-
-
-
